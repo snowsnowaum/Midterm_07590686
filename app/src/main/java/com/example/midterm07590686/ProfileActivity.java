10340e956f78fc968b90c9cbb770c0139868b54f
snowsnowaum1 <snowsnowaum01@gmail.com>
2019-09-08 12:01:41 +0700

    2

--- app/src/main/java/com/example/midterm07590686/ProfileActivity.java
+++ app/src/main/java/com/example/midterm07590686/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -12,13 +14,34 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-        /*Intent intent = getIntent();
+        Intent intent = getIntent();
         String username = intent.getStringExtra("user");
+
         if(username.equals("aaa")){
-            Toast.makeText(ProfileActivity.this,R.string.welcome+" "+username,Toast.LENGTH_SHORT).show();
-            //findViewById(R.id.textviewGoes).set;
+            Toast.makeText(ProfileActivity.this,R.string.welcome+" "+"Wittawin Kultummayotin",Toast.LENGTH_SHORT).show();
+            TextView setGoes = findViewById(R.id.textviewGoes);
+            TextView setName = findViewById(R.id.textViewName);
+            TextView setLives = findViewById(R.id.textviewLives);
+            TextView setFrom = findViewById(R.id.textviewFrom);
+            setName.setText(R.string.wittawin_kultummayotin);
+            setGoes.setText(R.string.rajabopit);
+            setLives.setText(R.string.bangkok);
+            setGoes.setText(R.string.bangkok);
+
+        }
+        else{
+            Toast.makeText(ProfileActivity.this,R.string.welcome+" "+"Promlert Lovichit",Toast.LENGTH_SHORT).show();
+            TextView setGoes = findViewById(R.id.textviewGoes);
+            TextView setName = findViewById(R.id.textViewName);
+            TextView setLives = findViewById(R.id.textviewLives);
+            TextView setFrom = findViewById(R.id.textviewFrom);
+            setName.setText("Promlert Lovichit");
+            setGoes.setText("สวนกุหลาบวิทยาลัย");
+            setLives.setText(R.string.bangkok);
+            setFrom.setText(R.string.bangkok);
+
         }
         //getIntent().getExtras();
-        */
+
     }
 }
